bae5ad47bd0fb12a882e4291572988ec32d2dad5
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-01 21:51:09 +0530

    product model to create new product and add it to file created

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,6 @@
-const products = require('../data/products') 
+let products = require('../data/products') 
+const { v4: uuidv4} = require('uuid')
+const { writeToFile } = require('../utils')
 
 const findAll = () => {
     return new Promise((resolve, reject) => {
@@ -12,7 +14,20 @@ const findById = (id) => {
     })
 }
 
+const create = (product) => {
+    return new Promise((resolve, reject) => {
+        const newProduct = {
+            id: uuidv4(), 
+            ...product
+        }
+        products.push(newProduct)
+        writeToFile('./data/products.json', products)
+        resolve(newProduct)
+    })
+}
+
 module.exports = {
     findAll,
-    findById
+    findById,
+    create
 }
