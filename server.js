e01dfd84ffe6da473554f4cbb032d8eb897aaed5
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-01 19:58:26 +0530

    rest api get method created on products route

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const http = require('http')
 const colors = require('colors')
 const morgan = require('morgan')
+const products = require('./data/products')
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>Hello beti logo!</h1>')
-    res.end()
-
+    if(req.url === "/api/v1/products" && req.method === "GET"){
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(products))
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({ message: '404, Route Not found!'}))
+    }
 })
+
 const PORT = process.env.PORT || 4000
-
-
 
 server.listen(PORT, () => {
     console.log(`Server started running on http://localhost:4000`.yellow.inverse)
