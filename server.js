d163d35681167a43263d93faa08c5542631b3663
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-02 18:30:25 +0530

    regex pattern change to accomodate uuid pattern along with integer ids

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http')
 const colors = require('colors')
 const products = require('./data/products')
-const { getAllProducts, getProductById, addNewProduct, deleteProduct } = require('./controllers/productController')
+const { getAllProducts, getProductById, addNewProduct, deleteProduct, updateProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
     if(req.url === "/api/v1/products" && req.method === "GET"){
         getAllProducts(req, res)
-    } else if (req.url.match(/\/api\/v1\/products\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)||([0-9]+)([0-9]+)/) && req.method === "GET") {
+    } else if (req.url.match(/\/api\/v1\/products\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)|([0-9]+)/) && req.method === "GET") {
         const id = req.url.split("/")[4]
         getProductById(req, res, id)
     } else if (req.url === "/api/v1/products" && req.method === "POST") {
@@ -14,7 +14,6 @@ const server = http.createServer((req, res) => {
     } else if (req.url.match(/\/api\/v1\/products\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)|([0-9]+)/) && req.method === "DELETE") {
         const id =  req.url.split('/')[4]
         deleteProduct(req, res, id)
-    }
     } else if (req.url.match(/\/api\/v1\/products\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)|([0-9]+)/) && req.method === "PUT") {
         const id =  req.url.split('/')[4]
         updateProduct(req, res, id)
