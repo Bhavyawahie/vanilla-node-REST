e0e7aaed6ece52da401761a5967337d495a7bc1e
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-11 23:13:36 +0530

    authMiddleware containing protect function to protect secured routed added

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,32 @@
 const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+
+const protect = async (req, res) => {
+    let token
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(' ')[2]
+            const decodedToken = jwt.verify(token, "secretKey")
+            const user = await User.findById(decodedToken.id)
+            const headerUserObject = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+            }
+            req.user = headerUserObject
+            return true
+        } catch (error) {
+            res.writeHead(401, {"Content-Type": "application/json"})
+            res.end(JSON.stringify({message: "Not authorized, User not found!"}))
+        }
+    }
+    if(!token){
+        res.writeHead(401, {"Content-Type": "application/json"})
+        res.end(JSON.stringify({message: "Not authorized, Token not found!"}))
+    }
+
+}
+
+module.exports = {
+    protect
+}
